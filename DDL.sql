8cf47c3e75b6804b120819aab8602f124b37a95c
Santiago <guti142000.sg@gmail.com>
2022-05-22 11:33:21 +0200

    modificación de DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -18,7 +18,7 @@ create table album(
     ano_creacion int not null,
     cod_art_owner int not null,
     constraint fk_artist FOREIGN key(cod_art_owner) 
-        references artista(cod_art) on delete cascade
+        references artista(cod_art) on delete cascade on update cascade
 );
 
 --Creación de la tabla de canción
@@ -33,9 +33,9 @@ create table cancion(
     cod_album int,
     cod_artist int not null,
     constraint fk_album FOREIGN key(cod_album)
-        references album(cod_alb) on delete set null deferrable initially deferred,
+        references album(cod_alb) on delete set null on update cascade deferrable initially deferred,
     constraint fk_artista FOREIGN key(cod_artist)
-        references artista(cod_art) on delete cascade
+        references artista(cod_art) on delete cascade on update cascade
 );
 
 
